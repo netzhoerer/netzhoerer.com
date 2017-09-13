752e9768b8d2a219c89f52cbce17d99ed5170121
Torsten Mirow <netzhoerer@gmail.com>
2017-09-13 23:54:40 +0200

    initiap commit

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,28 +1,55 @@
 /*
     ./webpack.config.js
 */
-const path = require('path');
 
+const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './src/index.html',
-  filename: 'index.html',
-  inject: 'body'
-});
 
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.jsx',
   output: {
     path: path.resolve('dist'),
-    filename: 'index.js'
+    filename: 'index.js',
+  },
+  resolve: {
+    modules: [
+      path.join(__dirname, 'src'),
+      'node_modules',
+    ],
+    extensions: ['.js', '.jsx', '.json', '.css', '.scss', '.png', '.jpg', '.jpeg', '.gif'],
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
-    ]
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ['react-hot-loader', /* 'eslint-loader', */'babel-loader'],
+        // 'react-hot-loader!babel-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader'],
+      },
+    ],
   },
-  plugins: [HtmlWebpackPluginConfig]
-
-}
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'body',
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    // new webpack.optimize.UglifyJsPlugin({
+    //  compress: {
+    //    warnings: false,
+    //  },
+    // }),
+  ],
+  devServer: {
+    contentBase: './dist',
+  },
+};
---
+++ src/TextDecoration/index.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+class TextDecoration extends Component {
+  constructor(props) {
+    super(props);
+    this.text = props.content;
+  }
+
+  state = {
+    typeOfNode: '',
+    hovered: 0,
+  }
+
+  mouseEnterHandler = () => {
+    this.setState({
+      typeOfNode: 'enter',
+      hovered: this.state.hovered < 10 ? this.state.hovered + 1 : 0,
+    });
+  }
+  mouseLeaveHandler = () => {
+    this.setState({
+      typeOfNode: 'i',
+    });
+  }
+
+  render() {
+    return (
+      <i
+        onMouseEnter={this.mouseEnterHandler}
+        onMouseLeave={this.mouseLeaveHandler}
+      >
+        {this.text}
+        <br />
+        <b>{this.state.typeOfNode}</b>
+        {
+          this.state.hovered > 0 &&
+          <span>
+            <br />
+            <u>hovered: {this.state.hovered}</u>
+          </span>
+        }
+      </i>
+    );
+  }
+}
+
+TextDecoration.propTypes = {
+  content: PropTypes.string,
+};
+
+TextDecoration.defaultProps = {
+  content: '',
+};
+
+export default TextDecoration;
